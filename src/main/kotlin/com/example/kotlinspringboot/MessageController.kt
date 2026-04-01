34cfa317cba3a8041957ce0ad05eee54239b5903
Gerardo Custodio <everythingissofast@gmail.com>
2026-04-01 14:59:56 -0600

    add findMessageById function on MessageController

--- src/main/kotlin/com/example/kotlinspringboot/MessageController.kt
+++ src/main/kotlin/com/example/kotlinspringboot/MessageController.kt
@@ -3,6 +3,7 @@ package com.example.kotlinspringboot
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,11 +15,19 @@ import java.net.URI
 @RequestMapping("/")
 class MessageController(private val service: MessageService) {
     @GetMapping
-    fun listMessages() = service.findMessages()
+    fun listMessages() = ResponseEntity.ok(service.findMessages())
 
     @PostMapping
     fun post(@RequestBody message: Message): ResponseEntity<Message> {
         val savedMessage = service.saveMessage(message)
         return ResponseEntity.created(URI("/${savedMessage.id}")).body(savedMessage)
     }
+
+    @GetMapping("/{id}")
+    fun getMessage(@PathVariable id: String): ResponseEntity<Message> =
+        service.findMessageById(id).toResponseEntity()
+
+    private fun Message?.toResponseEntity(): ResponseEntity<Message> =
+        //If the message is null (not found), set response code to 404
+        this?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
 }
--- src/main/kotlin/com/example/kotlinspringboot/MessageService.kt
+++ src/main/kotlin/com/example/kotlinspringboot/MessageService.kt
@@ -2,13 +2,18 @@ package com.example.kotlinspringboot
 
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
-import java.util.UUID
+import org.springframework.jdbc.core.query
+import java.util.*
 
 @Service
 class MessageService(private val db: JdbcTemplate) {
     fun findMessages(): List<Message> = db.query("SELECT * FROM messages") { response, _ ->
         Message(response.getString("id"), response.getString("text"))
     }
+
+    fun findMessageById(id: String): Message? = db.query("SELECT * FROM messages where id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }.singleOrNull()
 
     fun saveMessage(message: Message): Message {
         val id = message.id ?: UUID.randomUUID().toString() //generate id if it is null
