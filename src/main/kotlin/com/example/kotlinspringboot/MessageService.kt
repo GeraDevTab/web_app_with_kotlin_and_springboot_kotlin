7d4c1dbb19db9c419717c300df79f6df330ade8a
Gerardo Jimenez Custodio <gerardojcustodio@Mac-mini-de-Gerardo.local>
2026-03-31 22:06:41 -0600

    update MessageService and MessageController

--- src/main/kotlin/com/example/kotlinspringboot/MessageService.kt
+++ src/main/kotlin/com/example/kotlinspringboot/MessageService.kt
@@ -2,6 +2,7 @@ package com.example.kotlinspringboot
 
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class MessageService(private val db: JdbcTemplate) {
@@ -10,10 +11,11 @@ class MessageService(private val db: JdbcTemplate) {
     }
 
     fun saveMessage(message: Message): Message {
+        val id = message.id ?: UUID.randomUUID().toString() //generate id if it is null
         db.update(
             "INSERT INTO messages values ( ?, ?)",
-            message.id, message.text
+            id, message.text
         )
-        return message
+        return message.copy(id = id) //return a copy of the message with the new id
     }
 }
