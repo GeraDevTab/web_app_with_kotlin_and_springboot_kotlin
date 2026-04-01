358a6ed143dd272367d9fe3dc913a7a640cefba9
Gerardo Custodio <everythingissofast@gmail.com>
2026-04-01 15:43:26 -0600

    update MessageService to use MessageRepository instance

--- src/main/kotlin/com/example/kotlinspringboot/MessageService.kt
+++ src/main/kotlin/com/example/kotlinspringboot/MessageService.kt
@@ -1,26 +1,16 @@
 package com.example.kotlinspringboot
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.jdbc.core.query
 import java.util.*
 
 @Service
-class MessageService(private val db: JdbcTemplate) {
-    fun findMessages(): List<Message> = db.query("SELECT * FROM messages") { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }
+class MessageService(private val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
 
-    fun findMessageById(id: String): Message? = db.query("SELECT * FROM messages where id = ?", id) { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }.singleOrNull()
+    fun findMessageById(id: String): Message? = db.findByIdOrNull(id)
 
-    fun saveMessage(message: Message): Message {
-        val id = message.id ?: UUID.randomUUID().toString() //generate id if it is null
-        db.update(
-            "INSERT INTO messages values ( ?, ?)",
-            id, message.text
-        )
-        return message.copy(id = id) //return a copy of the message with the new id
-    }
+    fun saveMessage(message: Message): Message = db.save(message)
 }
